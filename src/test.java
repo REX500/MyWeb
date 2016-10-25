9c491bcf43ee12c91d2057a102d025ed949225c9
REX500 <filamalek@hotmail.com>
2016-10-25 21:35:06 +0200

    - div hide/show is made with jQuery now, added more divs and working on a mail contact form as it is. Should be done with it tomorrow

---
+++ src/test.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Filip on 25-10-2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
